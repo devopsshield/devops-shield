282d08e01fb3d94ff2a9bb3bfb8d289b58cbf304
Emmanuel K <emmanuel@devopsabcs.com>
2025-03-23 08:20:39 -0400

    Added copyright and license info

--- support/one-dos-vm/DNS/Update-LocalDns.ps1
+++ support/one-dos-vm/DNS/Update-LocalDns.ps1
@@ -1,8 +1,31 @@
+# MIT License
+
+# Copyright (c) 2025 DevOps Shield
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 param (
     [Parameter(Mandatory = $false)]
     [string]$ZoneName = "devopsabcs.com",
 
-    [string]$customerSuffix = "cx-003",
+    [string]$customerSuffix = "cx-002",
 
     [Parameter(Mandatory = $false)]
     [string[]]$CnameRecordNames = @("defectdojo-$customerSuffix", "sonarqube-$customerSuffix", "devopsshield-$customerSuffix", "dependencytrack-fe-$customerSuffix", "dependencytrack-api-$customerSuffix"),
@@ -11,12 +34,20 @@ param (
     [string]$DnsServer = "dc2devops.devopsabcs.com",
 
     [Parameter(Mandatory = $false)]
-    [string]$CnameRecordTarget = "onedosdev003.canadacentral.cloudapp.azure.com" #onedosvmcx003.devopsabcs.com
+    [string]$CnameRecordTarget = "onedosmngsvc002.devopsabcs.com" #"onedosdev003.canadacentral.cloudapp.azure.com" #onedosvmcx003.devopsabcs.com #
 )
 
 # # sample usage
-# .\Update-LocalDns.ps1 -customerSuffix "cx-003" `
-#     -CnameRecordTarget "onedosvmcx003.devopsabcs.com"
+# .\Update-LocalDns.ps1 -customerSuffix "cx-002" `
+#     -CnameRecordTarget "onedosmngsvc002.devopsabcs.com"
+
+Write-Output "Output parameters:"
+Write-Output "ZoneName: $ZoneName"
+Write-Output "customerSuffix: $customerSuffix"
+Write-Output "CnameRecordNames: $CnameRecordNames"
+Write-Output "DnsServer: $DnsServer"
+Write-Output "CnameRecordTarget: $CnameRecordTarget"
+
 
 # Verify if DnsServer PowerShell module is installed
 function Test-DnsServerModule {
